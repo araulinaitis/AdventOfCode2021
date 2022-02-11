74abe22867dd0185a9747333197f9bc78db29d63
araulinaitis <araulinaitis@gmail.com>
2022-02-11 11:51:26 -0800

    Day 21 part 2

--- D21P2/Player.js
+++ D21P2/Player.js
@@ -1,61 +1,64 @@
-import { counts } from './constants.js';
+import { winningScore } from './constants.js';
 
 export default class Player {
-  constructor(position, score = 0, count = 1, turnNum = 0) {
+  constructor(playerKey, position, score = 0, count = 1, turnNum = 0) {
     this.pos = position;
     this.count = count;
     this.score = score;
     this.children = [];
-    this.wonAt = this.score >= 1 ? turnNum : null;
+    this.wonAt = this.score >= winningScore ? turnNum : null;
     this.opponent = null;
     this.didLose = false;
+    this.playerKey = playerKey;
   }
-  
+
   setOpponent(opponent) {
     this.opponent = opponent;
   }
 
-  playTurn(turnNum) {
-    if (this.wonAt) return;
-    if (this.children.length) {
-      for (let child of this.children) {
-        child.playTurn(turnNum);
-      }
-    } else {
-      for (let [offset, count] of Object.entries(counts)) {
-        offset = parseInt(offset);
-        const newPos = ((this.pos + offset - 1) % 10) + 1;
-        this.children.push(new Player(newPos, this.score + newPos, this.count * count, turnNum));
-      }
-    }
+  copy() {
+    const newPlayer = new Player(this.playerKey, this.pos, 0, this.count, null, this.globalWinCounts, true);
+    newPlayer.score = this.score;
+    newPlayer.didLose = this.didLose;
+    newPlayer.wonAt = this.wonAt;
+    return newPlayer;
   }
 
-  splitForOtherTurn() {
-    // copy this one 3 times for the 3 universes created by the other player
-    if (this.children.length) {
-      for (let child of this.children) {
-        child.splitForOtherTurn();
-      }
-    } else if (!this.wonAt) {
-      this.count *= 3;
-    }
-  }
-
-  returnWonAt() {
-    let winCounts = {};
-    if (this.children.length) {
-      for (let child of this.children) {
-        const thisWinCount = child.returnWonAt();
-        for (let [turnNum, count] of Object.entries(thisWinCount)) {
-          if (!winCounts[turnNum]) winCounts[turnNum] = 0;
-          winCounts[turnNum] += count;
+  playTurn(turnNum, isOpponent = false) {
+    let newChildren = [this];
+    for (let roll = 0; roll < 3; ++roll) {
+      let newNewChildren = [];
+      for (let diceVal = 1; diceVal <= 3; ++diceVal) {
+        for (let newChild of newChildren) {
+          const newPos = ((newChild.pos + diceVal - 1) % 10) + 1;
+          const newPlayer = new Player(newChild.playerKey, newPos, this.score + newPos, newChild.count, turnNum, newChild.globalWinCounts);
+          newPlayer.opponent = newChild.opponent.copy();
+          newPlayer.opponent.opponent = newPlayer;
+          newNewChildren.push(newPlayer);
         }
       }
-    } else {
-      if (this.wonAt) {
-        winCounts[this.wonAt] = this.count;
-      }
+      newChildren = newNewChildren;
     }
-    return winCounts;
+
+    if (!isOpponent) {
+      let newNewChildren = [];
+      for (let newChild of newChildren) {
+        if (newChild.wonAt) {
+          newNewChildren.push(newChild);
+        } else {
+          const newOpponents = newChild.opponent.playTurn(turnNum, true);
+          for (let newOpponent of newOpponents) {
+            const thisNewChild = newChild.copy();
+            if (newOpponent.wonAt) thisNewChild.didLose = true;
+            thisNewChild.opponent = newOpponent;
+            newOpponent.opponent = thisNewChild;
+            newNewChildren.push(thisNewChild);
+          }
+        }
+      }
+      newChildren = newNewChildren;
+    }
+
+    return newChildren;
   }
 }
--- D21P2/main.js
+++ D21P2/main.js
@@ -1,57 +1,57 @@
 import * as fs from 'fs/promises';
-import { counts } from './constants.js';
 import Player from './Player.js';
 
-// let inputData = (await fs.readFile('./data/input.csv', { encoding: 'UTF-8' })).split('\r\n').map(entry => parseInt(entry.match(/Player [0-9]+ starting position: ([0-9]+)/)[1]));
-let inputData = (await fs.readFile('./data/testData.csv', { encoding: 'UTF-8' })).split('\r\n').map(entry => parseInt(entry.match(/Player [0-9]+ starting position: ([0-9]+)/)[1]));
+let inputData = (await fs.readFile('./data/input.csv', { encoding: 'UTF-8' })).split('\r\n').map(entry => parseInt(entry.match(/Player [0-9]+ starting position: ([0-9]+)/)[1]));
+// let inputData = (await fs.readFile('./data/testData.csv', { encoding: 'UTF-8' })).split('\r\n').map(entry => parseInt(entry.match(/Player [0-9]+ starting position: ([0-9]+)/)[1]));
 
 // console.log(inputData);
+// console.log(counts);
 
-console.log(counts);
+let winCounts = { 1: 0, 2: 0 };
 
-const player1 = new Player(inputData[0], 0, 1, 0);
-const player2 = new Player(inputData[1], 0, 1, 0);
+const player1 = new Player(1, inputData[0], 0, 1, 0);
+const player2 = new Player(2, inputData[1], 0, 1, 0);
 player1.setOpponent(player2);
 player2.setOpponent(player1);
+let player1s = [player1];
 for (let turnNum = 1; turnNum <= 21; ++turnNum) {
-  player1.playTurn(turnNum);
-  player2.splitForOtherTurn();
-  player2.playTurn(turnNum);
-  player1.splitForOtherTurn();
+  if (player1s.length === 0) break;
+  console.log({ turnNum });
+  let newPlayer1s = [];
+
+  for (let player1 of player1s) {
+    newPlayer1s = [...newPlayer1s, ...player1.playTurn(turnNum)];
+  }
+
+  console.log(newPlayer1s.length);
+  let combinedPlayer1s = [];
+  for (let newPlayer1 of newPlayer1s) {
+    if (newPlayer1.wonAt) {
+      winCounts[1] += newPlayer1.count;
+    } else if (newPlayer1.didLose) {
+      winCounts[2] += newPlayer1.opponent.count;
+    } else {
+      let found = false;
+      for (let combinedPlayer1 of combinedPlayer1s) {
+        if (
+          combinedPlayer1.pos === newPlayer1.pos &&
+          combinedPlayer1.score === newPlayer1.score &&
+          combinedPlayer1.opponent.pos === newPlayer1.opponent.pos &&
+          combinedPlayer1.opponent.score === newPlayer1.opponent.score
+        ) {
+          combinedPlayer1.count += newPlayer1.count;
+          combinedPlayer1.opponent.count += newPlayer1.opponent.count;
+          found = true;
+          break;
+        }
+      }
+      if (!found && !newPlayer1.wonAt && !newPlayer1.didLose) {
+        combinedPlayer1s.push(newPlayer1);
+      }
+    }
+  }
+  player1s = combinedPlayer1s;
+  console.log(player1s.length);
+  console.log({ winCounts });
 }
-
-const player1WinAts = player1.returnWonAt();
-const player2WinAts = player2.returnWonAt();
-console.log('Player 1: ', player1WinAts);
-console.log('Player 2: ', player2WinAts);
-
-let player1Wins = 0;
-let player2Wins = 0;
-
-for (let turnNum of Object.keys(player1WinAts)) {
-  player1Wins += player1WinAts[turnNum];
-  player2Wins += player2WinAts[turnNum] - player1WinAts[turnNum];
-}
-
-console.log(player1Wins, player2Wins);
-// console.log(player1.children[0].children[0]);
-
-// let rollNum = 0;
-// let diceVal = 1;
-// let scores = [0, 0];
-// while (scores.every(val => val < 1000)) {
-//   for (let player = 0; player < 2; ++player) {
-//     for (let roll = 0; roll < 3; ++roll) {
-//       // console.log(`${inputData[player]} + ${diceVal} => ${((inputData[player] + diceVal - 1) % 10 + 1)}`);
-//       inputData[player] = ((inputData[player] + diceVal - 1) % 10) + 1;
-//       diceVal++;
-//       rollNum++;
-//     }
-//     scores[player] += inputData[player];
-//     if (scores[0] >= 1000) break;
-//   }
-//   console.log(scores);
-// }
-// console.log(rollNum);
-// console.log(scores);
-// console.log(Math.min(...scores) * rollNum);
+console.log(winCounts);
